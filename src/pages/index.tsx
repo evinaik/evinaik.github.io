a2624630e92c0ec850172c47f9feb1acba6d65fa
Ekansh Vinaik <ekansh.vinaik@gmail.com>
2021-06-11 00:18:52 -0400

    feat: remove fade

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 const StyledLink = styled.a`
   text-decoration: none;
@@ -61,94 +61,79 @@ const Maryland = styled(WhiteLink)`
   background-color: #e03a3e;
 `;
 
-const fade = keyframes`
-  0% { opacity:0; }
-  66% { opacity:0; }
-  100% { opacity:1; }
-`;
-
-const FadeIn = styled.div<{ delay: number }>`
-  animation: ${(props) => props.delay}s ease 0s normal forwards 1 ${fade};
-  animation-iteration-count: 1;
-`;
-
 function IndexPage() {
   return (
     <React.Fragment>
-      <FadeIn delay={4}>
-        <p>
-          I'm a software engineer at{" "}
-          <TwoSigma target="_blank" href="https://www.twosigma.com/">
-            Two Sigma
-          </TwoSigma>
-          . I studied Computer Science at the{" "}
-          <Maryland target="_blank" href="https://www.umd.edu/">
-            University of Maryland, College Park
-          </Maryland>
-          .
-        </p>
-        <p>
-          In 2019, I interned at{" "}
-          <TwoSigma target="_blank" href="https://www.twosigmaiq.com/">
-            Two Sigma Insurance Quantified
-          </TwoSigma>{" "}
-          and{" "}
-          <Facebook target="_blank" href="https://www.facebook.com/">
-            Facebook
-          </Facebook>
-          . Previously, I've had a chance to contribute at{" "}
-          <CapitalOne target="_blank" href="https://www.capitalonelabs.com/">
-            Capital One Labs
-          </CapitalOne>
-          , and{" "}
-          <Amazon target="_blank" href="https://aws.amazon.com/">
-            Amazon Web Services
-          </Amazon>
-          .
-        </p>
-        <p>
-          At school, I completed research at the{" "}
-          <Maryland target="_blank" href="http://mbrc.umd.edu/">
-            Maryland Blended Reality Center
-          </Maryland>
-          . I specialized in virtual and augmented reality, including their
-          medical applications and streaming optimizations. I'm also an alumni
-          of the{" "}
-          <QUEST
-            target="_blank"
-            href="https://www.rhsmith.umd.edu/programs/undergraduate-programs/academics/fellows-special-programs/quest"
-          >
-            QUEST Honors Program
-          </QUEST>
-          , a program focusing on design and innovation.
-        </p>
-        <p>
-          Outside of that, I share code on{" "}
-          <GitHub target="_blank" href="https://github.com/evinaik/">
-            GitHub
-          </GitHub>
-          , where you'll find both completed projects and works in progress. For
-          example, I've published a multidisciplinary team designing a virtual
-          reality memory palace called{" "}
-          <DiscoVR target="_blank" href="https://github.com/evinaik/discovr">
-            DiscoVR
-          </DiscoVR>
-          . Feel free to connect with me on my{" "}
-          <LinkedIn target="_blank" href="https://linkedin.com/in/evinaik">
-            LinkedIn
-          </LinkedIn>{" "}
-          as well.
-        </p>
-      </FadeIn>
-      <FadeIn delay={6}>
-        <p>
-          Interested? Contact me at{" "}
-          <Email target="_blank" href="mailto:ekansh.vinaik@gmail.com">
-            ekansh.vinaik@gmail.com
-          </Email>
-          .
-        </p>
-      </FadeIn>
+      <p>
+        I'm a software engineer at{" "}
+        <TwoSigma target="_blank" href="https://www.twosigma.com/">
+          Two Sigma
+        </TwoSigma>
+        . I studied Computer Science at the{" "}
+        <Maryland target="_blank" href="https://www.umd.edu/">
+          University of Maryland, College Park
+        </Maryland>
+        .
+      </p>
+      <p>
+        In 2019, I interned at{" "}
+        <TwoSigma target="_blank" href="https://www.twosigmaiq.com/">
+          Two Sigma Insurance Quantified
+        </TwoSigma>{" "}
+        and{" "}
+        <Facebook target="_blank" href="https://www.facebook.com/">
+          Facebook
+        </Facebook>
+        . Previously, I've had a chance to contribute at{" "}
+        <CapitalOne target="_blank" href="https://www.capitalonelabs.com/">
+          Capital One Labs
+        </CapitalOne>
+        , and{" "}
+        <Amazon target="_blank" href="https://aws.amazon.com/">
+          Amazon Web Services
+        </Amazon>
+        .
+      </p>
+      <p>
+        At school, I completed research at the{" "}
+        <Maryland target="_blank" href="http://mbrc.umd.edu/">
+          Maryland Blended Reality Center
+        </Maryland>
+        . I specialized in virtual and augmented reality, including their
+        medical applications and streaming optimizations. I'm also an alumni of
+        the{" "}
+        <QUEST
+          target="_blank"
+          href="https://www.rhsmith.umd.edu/programs/undergraduate-programs/academics/fellows-special-programs/quest"
+        >
+          QUEST Honors Program
+        </QUEST>
+        , a program focusing on design and innovation.
+      </p>
+      <p>
+        Outside of that, I share code on{" "}
+        <GitHub target="_blank" href="https://github.com/evinaik/">
+          GitHub
+        </GitHub>
+        , where you'll find both completed projects and works in progress. For
+        example, I've published a multidisciplinary team designing a virtual
+        reality memory palace called{" "}
+        <DiscoVR target="_blank" href="https://github.com/evinaik/discovr">
+          DiscoVR
+        </DiscoVR>
+        . Feel free to connect with me on my{" "}
+        <LinkedIn target="_blank" href="https://linkedin.com/in/evinaik">
+          LinkedIn
+        </LinkedIn>{" "}
+        as well.
+      </p>
+      <p>
+        Interested? Contact me at{" "}
+        <Email target="_blank" href="mailto:ekansh.vinaik@gmail.com">
+          ekansh.vinaik@gmail.com
+        </Email>
+        .
+      </p>
     </React.Fragment>
   );
 }
